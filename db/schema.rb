5c856eb65b2c07ae1a6819ecb13e94ed1429c87a
Sam Brady <sambrady0652@gmail.com>
2021-10-13 13:21:33 -0500

    db fix

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_10_13_154133) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "hands", force: :cascade do |t|
     t.string "cards"
     t.string "value"
