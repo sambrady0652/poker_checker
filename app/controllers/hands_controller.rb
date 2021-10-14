d84a81152d92a045f01f42c502c6fb6330043dcf
Sam Brady <sambrady0652@gmail.com>
2021-10-14 16:49:23 -0500

    some final comments

--- app/controllers/hands_controller.rb
+++ app/controllers/hands_controller.rb
@@ -11,6 +11,7 @@ class HandsController < ApplicationController
   def create
     @hand = Hand.new(params.require(:hand).permit(:cards))
     @hand.value = CardEvaluator.new(cards: params.dig("hand","cards")).evaluate
+    # normally I would include some validation here before saving anything to DB
     @hand.save
     redirect_to @hand
   end
